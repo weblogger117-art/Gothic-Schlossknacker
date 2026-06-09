d5e19ee58c8e960ec123e5ea543e9e42e49e2467
weblogger117-art <weblogger117@gmail.com>
2026-06-09 14:41:11 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+
+const CACHE='gothic-schlossknacker-v64';
+const ASSETS=[
+ './',
+ './index.html',
+ './manifest.json',
+ './icon-192.png',
+ './icon-512.png',
+ './Splashscreen.png'
+];
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
